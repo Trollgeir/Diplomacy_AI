e5da337b06a6ac4656e81a525c3c4fee4628d88a
Reigertje <brianreigersberg@gmail.com>
2014-05-22 14:38:49 +0200

    he is it

--- src/dodo/DodoAI.java
+++ src/dodo/DodoAI.java
@@ -3,7 +3,7 @@ package dodo;
 import ai.AI;
 
 public class DodoAI implements AI {
-/* This AI is called Dodo as he has no natural enemies. Also, naive. */
+/* This AI is called Dodo as it has no natural enemies. Also, naive. */
 
 
 }
