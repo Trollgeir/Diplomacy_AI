c4d6bdf6397bdfa13948c412b951b1935630b386
DoctorBroccoli <koenvankatwijk@hotmail.com>
2014-05-22 14:16:43 +0200

    Filled in the Move class

--- src/kb/order/Move.java
+++ src/kb/order/Move.java
@@ -1,5 +1,28 @@
 package kb.order;
 
-public class Move {
+import kb.*;
+import kb.unit.*;
 
+/**
+ * "Move" is the order to move a unit to an adjacent node
+ * @author Koen
+ *
+ */
+
+public class Move implements Order {
+
+	Unit 		unit;
+	Node 		moveTo;
+	
+	Move(Unit _unit, Node _moveTo)
+	{
+		unit = _unit;
+		moveTo = _moveTo;
+	}
+	
+	public String translateToDAIDE()
+	{
+		return "(" + "Unit indicator here!" + ") MTO " + "Province indicator here!";
+	}
+	
 }
--- src/kb/order/Order.java
+++ src/kb/order/Order.java
@@ -3,4 +3,11 @@ package kb.order;
 
 public interface Order {
 
+	
+	/**
+	 * Translate this order to DAIDE, allowing the server to process the order.
+	 * @return The DAIDE version of this order.
+	 */
+	String translateToDAIDE();
+	
 }
