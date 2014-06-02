76b6b3d1129c0d13363a85396a6d6aba4f3b367b
jurdg <jjunk1111@gmail.com>
2014-06-02 14:13:25 +0200

    sender sends

--- src/communication/Sender.java
+++ src/communication/Sender.java
@@ -1,24 +1,49 @@
 package communication;
 
 import java.net.InetAddress;
+import server.Server;
 
 public class Sender
 {
-	InetAddress ip;
 	int port;
+	String name;
+	InetAddress ip;
+	Server serv;
+	
 
-	public Sender(InetAddress _ip /*host*/, int _port)
+	public Sender(InetAddress _ip, int _port, String _name)
 	{
 		this.ip = _ip;
 		this.port = _port;
 		
-		
+		this.name = _name;
+    	try {
+    	    serv = new Server(ip, port);
+    	    serv.addMessageListener(this);
+    	    serv.connect();
+    	    String[] msg = new String[]{
+    	    		"NME",
+                    "(", "'" + this.name + "'", ")",
+                    "(", "'" + VERSION + "'", ")"
+                  };
+    	    
+    	    serv.send(msg);
+    	    
+    	} catch (IOException ioe){
+    		ioe.printStackTrace();
+    	} catch (DisconnectedException de){
+    		System.out.println("Disconnected");
+    		System.exit(0);
+    	} catch (UnknownTokenException ute){
+    		System.err.println("Unknown token '" + ute.getToken() + "'");
+    		System.exit(1);
+    	} 
 		
 	}
 	
-	public void send(String message)
+	public void send(String[] message)
 	{
-		
+		serv.send(message);
 	}
 	
 }
