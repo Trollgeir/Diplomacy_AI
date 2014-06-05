ef9e309d965ba524f58eeebd0f27e2a58893e5b9
jurdg <jjunk1111@gmail.com>
2014-06-05 11:32:43 +0200

    AI njmuhytg5gtffgtuhj

--- src/ai/AI.java
+++ src/ai/AI.java
@@ -1,9 +1,26 @@
 package ai;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import communication.server.DisconnectedException;
+import communication.server.Server;
+import communication.server.UnknownTokenException;
+
 public abstract class AI {
 
-	public void onMessage(String[] message) {
-
+	public static void main(String[] args) throws UnknownHostException {
+			
+		Server serv = new Server(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+		try {
+			serv.send(args);
+		} catch (UnknownTokenException | DisconnectedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void onMessage(String[] message) {		
 	}
 
 }
--- src/kb/Map.java
+++ src/kb/Map.java
@@ -1,14 +1,9 @@
 package kb;
 
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
-import message.order.Order;
-import communication.Sender;
-import kb.province.Land;
 import kb.province.Province;
-import kb.unit.Army;
 
 /**
  * The map/knowledge base.
@@ -22,33 +17,6 @@ public class Map {
 	ArrayList<Power>		powers;
 	
 	public static void main(String[] args) throws UnknownHostException {
-		Sender send = new Sender(InetAddress.getByName("localhost"), 16713, "COOL");
-		Power power = new Power("POW");
-		Province p1, p2, p3;
-		Node n1, n2, n3;
-		p1 = new Land("NON", false);
-		n1 = new Node(p1);
-		
-		p2 = new Land("NTW", false);
-		n2 = new Node(p2);
-		
-		p3 = new Land("NTH", false);
-		n3 = new Node(p3);
-		
-		
-		n1.neighbors.add(n2);
-		n2.neighbors.add(n1);
-		
-		n2.neighbors.add(n3);
-		n3.neighbors.add(n2);
-		
-		n2.unit = new Army(power, n2);
-		ArrayList<Order> order = n2.unit.possibleOrders();
-		
-		for (int i = 0; i < order.size(); i++)
-		{
-			System.out.println(order.get(i).daide());
-		}
 	}
 
 	public Node getNode(String name)
