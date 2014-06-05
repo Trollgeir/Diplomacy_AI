4c1f8b5d9b578026aed01e493b297f1999f8a52f
jurdg <jjunk1111@gmail.com>
2014-06-05 14:48:20 +0200

    Draw for PDA added

--- src/message/server/Draw.java
+++ src/message/server/Draw.java
@@ -9,15 +9,29 @@ import message.DaideMessage;
 
 public class Draw implements DaideMessage{
 
+	String[] powers = null;
 	public Draw()
 	{
 		// Again no parameters
 	}
 
+	public Draw(String[] powers)
+	{
+		// Partial Draw Allowed?
+		// you can declare a draw with certain powers
+		this.powers = powers;
+	}
+	
 	@Override
 	public DaideList daide() {
 		DaideList result = new DaideList();
 		result.add("DRW");
+		if(this.powers != null)
+		{
+			result.add("(");
+			result.add2(this.powers);
+			result.add(")");
+		}			
 		return result;
 	}
 }
