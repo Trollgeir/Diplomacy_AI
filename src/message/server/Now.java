80ccfd390d79e9275dbcd682aa09166ad34f6225
jurdg <jjunk1111@gmail.com>
2014-06-05 14:18:29 +0200

    added SCO and NOW

---
+++ src/message/server/Now.java
@@ -0,0 +1,23 @@
+/*
+ * Ask the server for the starting positions for the "variant"
+ */
+
+package message.server;
+
+import message.DaideList;
+import message.DaideMessage;
+
+public class Now implements DaideMessage {
+	public Now()
+	{
+		// No parameters are needed
+	}
+	
+	@Override
+	public DaideList daide()
+	{
+		DaideList result = new DaideList();
+		result.add("NOW");
+		return result;
+	}
+}
