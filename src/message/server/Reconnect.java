703bb5600ea62e0372f4c303045293ac7ed79cdb
TheAntronach <t_de_goey@hotmail.com>
2014-06-05 14:03:58 +0200

    IAM message (rejoin after connection loss)

---
+++ src/message/server/Reconnect.java
@@ -0,0 +1,33 @@
+package message.server; 
+
+import kb.Power;
+import message.DaideList;
+import message.DaideMessage;
+
+public class Reconnect implements DaideMessage {
+
+	/**
+		Message class to ask the server to rejoin the game after connection loss (IAM)
+	**/
+
+	private Power power;
+	private String passcode;
+
+	public Reconnect(Power power, String passcode) {
+		this.power = power; 
+		this.passcode = passcode;
+	} 
+
+	@Override
+	public DaideList daide() {
+		DaideList result = new DaideList();
+		
+        result.add2("IAM","(");
+        result.addAll(power.daide());
+        result.add2(")");
+        result.add2("(", "'" + passcode + "'", ")");
+
+		return result;
+	}
+}
+
