39e7b0863fc5d6efaa8a1f5b957e17330c75405a
DoctorBroccoli <koenvankatwijk@hotmail.com>
2014-06-05 14:40:08 +0200

    Added Not

---
+++ src/message/server/Not.java
@@ -0,0 +1,29 @@
+package message.server; 
+
+import message.DaideList;
+import message.DaideMessage;
+
+public class Not implements DaideMessage {
+
+	/**
+		Message class to un-confirm messages
+	**/
+
+	private String[] message;
+
+	public Not(String[] message) {
+		this.message = message; 
+	} 
+
+	@Override
+	public DaideList daide() {
+		DaideList result = new DaideList();
+		
+        result.add2("NOT", "(");
+        result.add2(message);
+        result.add2(")");
+
+		return result;
+	}
+}
+
