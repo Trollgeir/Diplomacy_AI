4544066b34ec61da1b71a1e3221c6631e8802595
Reigertje <brianreigersberg@gmail.com>
2014-06-05 11:34:27 +0200

    moved main

--- src/ai/AI.java
+++ src/ai/AI.java
@@ -9,17 +9,6 @@ import communication.server.UnknownTokenException;
 
 public abstract class AI {
 
-	public static void main(String[] args) throws UnknownHostException {
-			
-		Server serv = new Server(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
-		try {
-			serv.send(args);
-		} catch (UnknownTokenException | DisconnectedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
 	public void onMessage(String[] message) {		
 	}
 
--- src/dodo/DodoAI.java
+++ src/dodo/DodoAI.java
@@ -5,7 +5,15 @@ import ai.AI;
 public class DodoAI extends AI {
 /* This AI is called Dodo as it has no natural enemies. Also, naive. */
 
-	public static void main(int argc, String args[]) {
-		/* create server/dispatcher/ai etc */
+	public static void main(String[] args) throws UnknownHostException {
+			
+		Server serv = new Server(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+		try {
+			serv.send(args);
+		} catch (UnknownTokenException | DisconnectedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
 }
