6d742c149d9d3f8ef0dc991c0f2d7c7d59bb12b6
Koen van Katwijk <koenvankatwijk@hotmail.com>
2014-06-17 09:50:00 +0200

    Added a threat-level to the beliefbase

--- src/ai/dodo/DodoBeliefBase.java
+++ src/ai/dodo/DodoBeliefBase.java
@@ -1,21 +1,124 @@
 package ai.dodo;
 
+import java.util.ArrayList;
+
 import kb.Map;
+import kb.Node;
+import kb.Power;
 import kb.province.Province;
+import kb.unit.Unit;
 
 public class DodoBeliefBase {
 	Map		map;
+	Power	self;
 	
-	java.util.Map<Province, Integer>	threat;
-	
-	
-	public DodoBeliefBase(Map map)
+	public java.util.Map<Province, Double>		threat;
+	public java.util.Map<Province, Double>		threatChange;
+	public java.util.Map<Power, Double>			trust;
+		
+	public DodoBeliefBase(Map map, Power self)
 	{
 		this.map = map;
-		//threat = new java.util.Map<Province, Integer>();
+		this.self = self;
+		
+		threat = new java.util.HashMap<Province, Double>();
+		threatChange = new java.util.HashMap<Province, Double>();
+		trust = new java.util.HashMap<Power, Double>();
 	}
 	
+	public int distance(Node start, Node goal)
+	{
+		java.util.Map<Node, Integer> distanceList = new java.util.HashMap<Node, Integer>();
+		ArrayList<Node> procdList = new ArrayList<Node>();
+		ArrayList<Node> addedList = new ArrayList<Node>();
+		
+		addedList.add(start);
+		distanceList.put(start, 0);
+		
+		for (int j = 0; j < addedList.size(); j++)
+		{
+			Node cNode = addedList.get(j);
+			
+			for (int i = 0; i < cNode.landNeighbors.size(); i++)
+			{
+				Node adjNode = cNode.landNeighbors.get(i);
+				
+				if (adjNode.equals(goal))
+					return distanceList.get(cNode) + 1;
+				
+				if (!distanceList.containsKey(adjNode))
+				{
+					distanceList.put(adjNode, distanceList.get(cNode) + 1);
+					addedList.add(adjNode);
+				}
+			}
+			procdList.add(cNode);
+		}
+		
+		return -1;
+	}
 	
+	public void calcThreats()
+	{
+		threatChange.clear();
+		
+		ArrayList<Province> ownedProvinces = map.getProvincesByOwner(self);
+		
+		java.util.HashMap<Province, Double> nThreat = new java.util.HashMap<Province, Double>();
+		for (int t = 0; t < ownedProvinces.size(); t++)
+		{
+			if (threat.containsKey(ownedProvinces.get(t)))
+			{
+				nThreat.put(ownedProvinces.get(t), threat.get(ownedProvinces.get(t)));
+			}
+		}
+		threat = nThreat;
+		
+		for (int p = 0; p < ownedProvinces.size(); p++)
+		{
+			Province cProv = ownedProvinces.get(p);
+			
+			double prevThreat;
+			if (threat.containsKey(cProv))
+				prevThreat = threat.get(cProv);
+			else
+				prevThreat = -1.0;
+				
+			double threatVal = 0.0;
+			
+			for (int pow = 0; pow < map.powers.size(); pow++)
+			{
+				Power cPow = map.powers.get(pow);
+				if (cPow.equals(self))
+					continue;
+				
+				ArrayList<Unit> threatUnits = map.getUnitsByOwner(cPow);
+				
+				for (int u = 0; u < threatUnits.size(); u++)
+				{
+					//double trustFactor = 1.0 - trust.get(cPow);
+					double trustFactor = 1.0;
+					int dist = distance(cProv.getCentralNode(), threatUnits.get(u).location);
+					
+					double threatFromUnit;
+					
+					if (dist != -1)
+						threatFromUnit = trustFactor * Math.pow(0.5, dist);
+					else
+						threatFromUnit = 0.0;
+					
+					threatVal += threatFromUnit;
+				}
+			}
+			threat.put(cProv, threatVal);
+			if (prevThreat != -1.0)
+				threatChange.put(cProv, threatVal - prevThreat);
+			else
+				threatChange.put(cProv, 0.0);
+		}
+		
+		System.out.println(threat);
+	}
 	
 	
 }
