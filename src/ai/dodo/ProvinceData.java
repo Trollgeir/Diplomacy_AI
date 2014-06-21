2a284d42b9f46831eed33de0a35a87189d6ddb13
TheAntronach <t_de_goey@hotmail.com>
2014-06-21 14:15:58 +0200

    Revert "new AI"
    This reverts commit d872569407f72bab1d53ece706d26fc7b3bb2bc6.

--- src/ai/dodo/ProvinceData.java
+++ src/ai/dodo/ProvinceData.java
@@ -1,8 +1,6 @@
 package ai.dodo;
 
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Collections;
 import java.lang.Comparable;
 import kb.Node;
 import kb.unit.Unit; 
@@ -13,10 +11,10 @@ public class ProvinceData {
 
 	public static float c_supply = 2.0f;
 	public static float c_adjSupply = 1.0f;
-	public static float c_shared = 0.0f;
+	public static float c_shared = 0.75f;
 	public static float risk = 0.75f; 
 	public static float sharedRisk = 0.5f;
-	public static float defence = 1.0f; 
+	public static float defence = 0.0f; 
 
 	public int supplyType; 
 	public int neighborSupply; 
@@ -26,8 +24,8 @@ public class ProvinceData {
 	public ArrayList<Power> powers;
 
 	public ArrayList<Province> adjProvinces; 
-	public ArrayList<ArrayList<Node>> destNodes; 
-	public ArrayList<ArrayList<UnitData>> nearUnits;
+	public ArrayList<ArrayList<Unit>> nearUnits;
+	public ArrayList<ArrayList<Node>> unitNodes;
 	public int[] shared;  
 
 	public int support; 
@@ -45,23 +43,13 @@ public class ProvinceData {
 		this.powers = powers; 
 
 		adjProvinces = new ArrayList<Province>();
-		nearUnits = new ArrayList<ArrayList<UnitData>>();
-		destNodes = new ArrayList<ArrayList<Node>>();
-		
-		for (int i = 0; i < powers.size(); ++i) {
-			nearUnits.add(new ArrayList<UnitData>());	
-			destNodes.add(new ArrayList<Node>());
-		}
+		nearUnits = new ArrayList<ArrayList<Unit>>();
+		for (int i = 0; i < powers.size(); ++i) nearUnits.add(new ArrayList<Unit>());	
 		shared = new int[powers.size()];
 
-		computeAdjProvinces();
-		System.out.println("Province: " + province.daide() + "with adjecent provinces:");
-		for (Province p : adjProvinces) {
-			System.out.println("\t" + p.daide());
-		}
-
-		addNearUnits(province);
-		for (Province p : adjProvinces) addNearUnits(p);
+		computeAdjProvinces();	
+		addNearUnit(province);
+		for (Province p : adjProvinces) addNearUnit(p);
 
 		computeGains(power);
 	}
@@ -73,50 +61,21 @@ public class ProvinceData {
 		computeWeight(power);
 	}
 
-	//remove the used units from nearUnits 
-	public void update(ArrayList<Unit> usedUnits) {
-		
-		int idx = powers.indexOf(power);
-		ArrayList<UnitData> removeList = new ArrayList<UnitData>();
-
-		//check whether a nearUnit is the same a usedUnit.
-		for (Unit unit : usedUnits) {
-			for (UnitData unitData : nearUnits.get(idx)) {
-				if (unit == unitData.unit) {
-					support--;
-					//if this unit was shared then the shared state needs to be decremented.
-					if (unitData.shared > 0) shared[idx]--;
-				 	
-				 	//remove the unit from the nearUnits
-					removeList.add(unitData);
-				}
-			}
-		} 
-
-		nearUnits.get(idx).removeAll(removeList);
-	}
-
 	public boolean isTakeable() {
-		if (mainEnemy == -1) {
-			return support > 0;
-		}
 		return support > risk * (maxNegSupport - shared[mainEnemy]) + sharedRisk * shared[mainEnemy]; 
 	}
 
-	//find all the adjecent provinces
+
 	private void computeAdjProvinces() {
 		ArrayList<Node> nodes = new ArrayList<Node>();
 		nodes.add(province.getCentralNode());
 		nodes.addAll(province.coastLine);
 
-		//loop through all the nodes our province contains.
 		for (Node node : nodes) {
-			//add the reachable provinces of node
 			computeAdjProvinces(node);
 		}
 	}
 
-	//add all the adjecent provinces of node to adjProvinces 
 	private void computeAdjProvinces(Node node) {
 		ArrayList<Node> nodes = new ArrayList<Node>();
 		nodes.addAll(node.landNeighbors);
@@ -124,7 +83,6 @@ public class ProvinceData {
 
 	 	for (Node n : nodes) {
 			Province adjProv = n.province;
-			//Ignore our own province and province which are already found
 			if (adjProv != province && !adjProvinces.contains(adjProv)) {
 				adjProvinces.add(adjProv); 
 			}
@@ -152,97 +110,36 @@ public class ProvinceData {
 		}
 	}
 
-	//Add the units from nearProv to nearUnits.
-	//This is done seperatly for each power. 
-	private void addNearUnits(Province nearProv) {
-		System.out.println("Adding near units of " + nearProv.daide() + "to " +  province.daide() );
+	private void addNearUnit(Province province) {
 		ArrayList<Node> nodes = new ArrayList<Node>();
-		nodes.add(nearProv.getCentralNode());
-		nodes.addAll(nearProv.coastLine);
+		nodes.add(province.getCentralNode());
+		nodes.addAll(province.coastLine);
 		
-		//Search through each node for a unit.
 		for (Node node : nodes) {
 			if (node.unit != null) {
 				Unit unit = node.unit;
+				Power p = unit.owner; 
 				
-				//add a new UnitData to nearUnits.
-				if (!containsUnit(unit)) {
-					//destNode is the node by which this unit can reach this province.
-					Node destNode = findDestNode(node, province);
-					if (destNode != null) {
-						int powerIdx = powers.indexOf(unit.owner);
-						nearUnits.get(powerIdx).add(new UnitData(unit, destNode));
+				for (int i = 0; i < powers.size(); ++i) {
+					if (p == powers.get(i) && !nearUnits.get(i).contains(unit)) {
+						nearUnits.get(i).add(unit);
+						return;
 					}
-					
 				}
 
-				//stop searching when a unit is found, also when it's not already contained by nearUnits. 
-				//since each province can contain maximal one unit. 
 				return; 
 			}
 		}		
 	}
 
-	private Node findDestNode(Node src, Province destProv) {	
-		//add to destNodes all the nodes that are reachable from src 		
-		ArrayList<Node> destNodes = getAdjNodes(src);
-		for (Node dest : destNodes) {
-			if (dest.province == destProv) return dest;
-		}
-
-		System.out.println("No node found at findDestNode!!!");
-		return null;
-	}
-
-	private boolean containsUnit(Unit unit) {
-		Power p = unit.owner; 
-		int powerIdx = powers.indexOf(p);
-
-		for (UnitData unitData : nearUnits.get(powerIdx)) {
-			if (unitData.unit == unit) return true;
-		}
-
-		return false;
-	}
-
-	private ArrayList<Node> getAdjNodes(Node node) {
-		ArrayList<Node> nodes = new ArrayList<Node>();
-		nodes.addAll(node.allNeighbors());
-		nodes.add(node); 
-		ArrayList<Node> list = new ArrayList<Node>();
-		
-		for (Node n : nodes) {
-
-			System.out.println(n.daide().toString());
-
-
-			if (node.isLand()) {
-				if (n.isLand()) list.add(n);
-			} else if (node.isSea()) {
-				if (n.isSea() || n.isCoast()) list.add(n);
-			} else if (node.isCoast()) {
-				if (n.isCoast() || n.isSea()) list.add(n);
-			} else {
-				System.out.println("none of them?");
-				if (!(node.isLand()|| node.isSea()|| node.isCoast())) System.out.println("noooooooooooooooooooooooooooooo");
-			}
-
-		}
-
-		return list; 
-	}
-
 	private void computeGains(Power power) {
-		//gains = c_supply * getSupplyWeight(province) * isOwnCenter(province, power) * defence;
-		gains = c_supply * getSupplyWeight(province);
+		gains = c_supply * getSupplyWeight(province) * isOwnCenter(province, power) * defence;
 		
 		for (Province p : adjProvinces) {
-			//gains += c_adjSupply  * getSupplyWeight(p) * isOwnCenter(province, power) * defence; 
-			gains += c_adjSupply  * getSupplyWeight(p);
+			gains += c_adjSupply  * getSupplyWeight(p) * isOwnCenter(province, power) * defence; 
 		}
 	}
 
-	//TODO: take main supply in account
 	private float getSupplyWeight(Province province) {
 		if (province.isSupplyCenter()) {
 			return 1.0f;
@@ -256,97 +153,37 @@ public class ProvinceData {
 	}
 
 	private void computeWeight(Power power) {
-		if (!isTakeable()) {
-			weight = 0;
-			return;
-		}
+		int idx = powers.indexOf(power);
 
 		//init weight on the gains.
 		weight = gains; 
 	
 		if (mainEnemy != -1) {
-			int idx = powers.indexOf(power);
-
 			//expected negative support
 			float expectedNeg =  risk * (maxNegSupport - shared[mainEnemy]) + sharedRisk * shared[mainEnemy];
 
 			//penalty for each unit that needs to support while it is shared.
-			int numFree = support - shared[idx];
-			weight -= c_shared * Math.max(expectedNeg - numFree, 0);		
+			weight -= c_shared * Math.max(expectedNeg - shared[idx], 0);		
 		}	
 	}
 
-	public void determineSharedUnits(ArrayList<ProvinceData> provinces) {
-		for (ProvinceData p : provinces) {
-			if (this == p) continue;
-
-			//Ignore provinces with no gains.
-			if (p.gains == 0) continue;
+	public static void determineSharedUnits(ArrayList<ProvinceData> provinces) {
+		for (ProvinceData p0 : provinces) {
+			if (p0.gains == 0) continue;
 			
-			//check for all unit lists of all the powers.
-			for (ArrayList<UnitData> subList : nearUnits) {
-				
-				for (UnitData unitData : subList) {  
-					int powerIdx = powers.indexOf(power);
+			for (ProvinceData p1 : provinces) {
+				if (p0 == p1 || p1.gains == 0) continue;
 
-					//contains can be used since equals() is overwritten
-					if (p.nearUnits.get(powerIdx).contains(unitData)) {
-						shared[powerIdx]++;
-						unitData.shared++;
+				for (ArrayList<Unit> subList :  p0.nearUnits) {
+					for (Unit u : subList) {
+						int powerIdx = p0.powers.indexOf(u.owner);
+
+						if (p1.nearUnits.get(powerIdx).contains(u)) {
+							p0.shared[powerIdx]++;
+						}
 					}
 				}
 			}
-			
-		}
-	}
-
-	
-	public ArrayList<Unit> getAvailebleUnits() {
-		int powerIdx = powers.indexOf(power);
-		ArrayList<UnitData> sortedList = new ArrayList<UnitData> ();
-		sortedList.addAll(nearUnits.get(powerIdx));
-
-		Collections.sort(sortedList,
-					new Comparator<UnitData>(){
-   						public int compare(UnitData u0, UnitData u1) {
-        					return u0.shared -  u1.shared;
-        				}
-        			});
-
-		ArrayList<Unit> result = new ArrayList<Unit>();
-		for (UnitData unitData : sortedList) {
-			result.add(unitData.unit);
-		}
-
-		return result; 
-	}
-	
-	public String toString() {
-		String str = "";
-		str += "\tname:    " + province.daide().toString() + "\n";
-		str += "\tgains:   " + gains + "\n";
-		str += "\tweight:  " + weight + "\n";
-		str += "\tsupport: " + support + "\n";
-		str += "\tmaxNegSupport: " + maxNegSupport + "\n"; 
-		return str; 
-	}
-
-	class UnitData {
-
-		public Unit unit;
-		public int shared;
-		public Node destNode;
-
-		public UnitData(Unit unit, Node destNode) {
-			this.unit = unit;
-			this.destNode = destNode; 
-			shared = 0;
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			UnitData u = (UnitData)o; 
-			return u.unit == unit; 
 		}
 	}
 }
